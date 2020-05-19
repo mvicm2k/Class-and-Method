3e01b84c9c0b3c3cabf827836711e84fd921781e
Efemena <efemena.onomarho@gmail.com>
2020-05-18 19:33:33 -0700

    Add project files.

---
+++ MyClass/Laptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    class Laptop:Owner
+    {
+        public string Brand;
+        public string Color;
+        public double Speed;
+        public int DiskSize;
+        public int Ram;
+
+        public Laptop(string aBrand, string aColor, double aSpeed, int aDiskSize, int aRam)
+        {
+            Brand = aBrand;
+            Color = aColor;
+            Speed = aSpeed;
+            DiskSize = aDiskSize;
+            Ram = aRam;
+
+
+        }
+
+    }
+}
---
+++ MyClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop("HP", "Ash", 3.5, 1, 12);
+            laptop.Name = "Victor";
+            var Mesage = laptop.Name + " has a " + laptop.Color + " Color " + laptop.Brand + " Laptop with a speed of " + laptop.Speed + "GHZ, " + laptop.Ram
+                + "GB RAM and a Hard disk of " + laptop.DiskSize + " tarabyte.";
+            Console.WriteLine(Mesage);
+
+            Console.ReadLine();
+        }
+    }
+}
